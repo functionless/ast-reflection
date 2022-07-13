725abfeb2ef0e5ec917e3c50010ce2bcdf97c0e9
sam <sam@functionless.org>
2022-07-13 16:21:30 -0700

    chore: document SyntaxContext id

--- tests/fixture/wrap-closure/input.js
+++ tests/fixture/wrap-closure/input.js
@@ -1,4 +1,4 @@
-let i = 0;
+let i = 0; // let i#1 = 0
 const a = () => {
   i;
 };
@@ -6,6 +6,14 @@ function _a() {
   i;
 }
 const b = () => {
-  let i = 0;
+  let i = 0; // let i#2 = 0
   i; // not a free variable since it is shadowed within this scope
+};
+
+const c = () => {
+  let i = 0; // let i#3 = 0
+
+  const d = () => {
+    i;
+  };
 };
--- tests/fixture/wrap-closure/output.js
+++ tests/fixture/wrap-closure/output.js
@@ -3,7 +3,7 @@ global.__fnl_func(_a, () => ({
   free: [["i", 1, i]],
 }));
 
-let i = 0;
+let i = 0; // let i#1 = 0
 const a = global.__fnl_func(
   () => {
     i;
@@ -18,8 +18,27 @@ function _a() {
 }
 const b = global.__fnl_func(
   () => {
-    let i = 0;
+    let i = 0; // let i#2 = 0
     i; // not a free variable since it is shadowed within this scope
+  },
+  () => ({
+    __filename,
+    free: [],
+  })
+);
+
+const c = global.__fnl_func(
+  () => {
+    let i = 0; // let i#3 = 0
+    const d = global.__fnl_func(
+      () => {
+        i;
+      },
+      () => ({
+        __filename,
+        free: [["i", 3, i]],
+      })
+    );
   },
   () => ({
     __filename,
