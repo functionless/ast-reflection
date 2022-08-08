c1d3b416341f42b2f77e34af2942d08a092825d1
sam <sam@functionless.org>
2022-08-07 23:14:33 -0700

    chore: set up projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,15 @@
 const { javascript } = require("projen");
 const project = new javascript.NodeProject({
+  name: "@functionless/ast-reflection",
+  main: "dist/ast_reflection.wasm",
   defaultReleaseBranch: "main",
-  name: "ast-reflection",
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  jest: false,
+  release: true,
+  releaseToNpm: true,
 });
+
+project.compileTask.exec(
+  "rm -rf dist && mkdir dist && cargo build --release --target wasm32-wasi && cp target/wasm32-wasi/release/ast_reflection.wasm ./dist/"
+);
+
 project.synth();
