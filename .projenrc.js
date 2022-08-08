0cc64fc90944834cd05ac5c7a0238614951e80d0
sam <sam@functionless.org>
2022-08-07 23:24:54 -0700

    fix: release

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,27 @@
 const { javascript } = require("projen");
 const project = new javascript.NodeProject({
   name: "@functionless/ast-reflection",
-  main: "dist/ast_reflection.wasm",
+  main: "ast_reflection.wasm",
   defaultReleaseBranch: "main",
   jest: false,
   release: true,
   releaseToNpm: true,
+  gitignore: ["/dist/"],
 });
 
+project.addPackageIgnore("/.gitattributes");
+project.addPackageIgnore("/target/");
+project.addPackageIgnore("/tests/");
+project.addPackageIgnore("/src/");
+project.addPackageIgnore("/.github/");
+project.addPackageIgnore("/Cargo.lock");
+project.addPackageIgnore("/Cargo.toml");
+project.addPackageIgnore("/rust-toolchain");
+project.addPackageIgnore("/rustfmt.toml");
+project.addPackageIgnore("/dist/");
+
 project.compileTask.exec(
-  "rm -rf dist && mkdir dist && cargo build --release --target wasm32-wasi && cp target/wasm32-wasi/release/ast_reflection.wasm ./dist/"
+  "cargo build --release --target wasm32-wasi && cp target/wasm32-wasi/release/ast_reflection.wasm ."
 );
 
 project.synth();
