06936e6382af7fb2a95236b5b54be9aba4c44c79
sam <sam@functionless.org>
2022-08-08 00:18:00 -0700

    chore(release): set up projen (#3)

---
+++ .projenrc.js
@@ -0,0 +1,43 @@
+const { javascript } = require("projen");
+const project = new javascript.NodeProject({
+  name: "@functionless/ast-reflection",
+  main: "ast_reflection.wasm",
+  defaultReleaseBranch: "main",
+  jest: false,
+  release: true,
+  releaseToNpm: true,
+  gitignore: ["/dist/", "/target/", "/ast_reflection.wasm"],
+  workflowBootstrapSteps: [
+    {
+      name: "Install rust",
+      uses: "actions-rs/toolchain@v1",
+      with: {
+        toolchain: "stable",
+        components: "rustfmt",
+        profile: "minimal",
+        override: true,
+      },
+    },
+    {
+      name: "Install wasm32-wasi",
+      run: "rustup target add wasm32-wasi",
+    },
+  ],
+});
+
+project.addPackageIgnore("/.gitattributes");
+project.addPackageIgnore("/target/");
+project.addPackageIgnore("/tests/");
+project.addPackageIgnore("/src/");
+project.addPackageIgnore("/.github/");
+project.addPackageIgnore("/Cargo.lock");
+project.addPackageIgnore("/Cargo.toml");
+project.addPackageIgnore("/rust-toolchain");
+project.addPackageIgnore("/rustfmt.toml");
+project.addPackageIgnore("/dist/");
+
+project.compileTask.exec(
+  "cargo build --release --target wasm32-wasi && cp target/wasm32-wasi/release/ast_reflection.wasm ."
+);
+
+project.synth();
