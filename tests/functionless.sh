76eef1ddfee45f859b5dd022565911c0a4df0db5
sam <sam@functionless.org>
2022-08-09 18:25:18 -0700

    chore: add a test that runs the functionless test:fast with the local version of ast-reflection

---
+++ tests/functionless.sh
@@ -0,0 +1,22 @@
+yarn link
+
+ROOT_DIR=$(pwd)
+TEST_DIR="${ROOT_DIR}/.test"
+
+mkdir -p ${TEST_DIR}
+
+clean_up() {
+  rm -rf ${TEST_DIR}
+}
+
+trap clean_up EXIT
+
+cd .test
+
+git clone --depth 1 git@github.com:functionless/functionless.git
+
+cd functionless
+yarn
+yarn link @functionless/ast-reflection
+yarn compile
+yarn test:fast
