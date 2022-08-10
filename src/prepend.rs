2270be2746c0462917eb74c531b202acf33c445b
sam <sam@functionless.org>
2022-08-10 15:55:17 -0700

    fix: prepend implementation

--- src/prepend.rs
+++ src/prepend.rs
@@ -4,5 +4,5 @@ use std::iter;
  * Prepends an element to the front of a Vec.
  */
 pub fn prepend<T>(to: &mut Vec<T>, element: T) {
-  *to = to.splice(0..0, iter::once(element)).collect();
+  to.splice(0..0, iter::once(element)).for_each(|_| {})
 }
